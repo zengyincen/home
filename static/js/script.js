ba7a0a70a2e28ab749eece43dd9b876190489c82
鹿 <70961031+deerwan@users.noreply.github.com>
2025-01-10 10:30:49 +0800

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-console.log('%cCopyright © 2024 zyyo.net',
+console.log('%cCopyright © 2024 deer.net',
     'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
 );
 console.log('%c   /\\_/\\', 'color: #8B4513; font-size: 20px;');
@@ -163,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    var pageLoading = document.querySelector("#zyyo-loading");
+    var pageLoading = document.querySelector("#deer-loading");
     setTimeout(function () {
         pageLoading.style.opacity = '0';
 
